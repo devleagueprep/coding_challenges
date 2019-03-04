c2d5e7d33bbe03fb71b707a98f3cfb6a3adcc29a
madycakes <viclee02@gmail.com>
2019-03-04 09:58:22 -1000

    adds exercises

---
+++ exercises.js
@@ -0,0 +1,23 @@
+// Function -  FirstFactorial(num)
+// The function will take the num parameter being passed and return the factorial of it (ie. if num =3, return(3 * 2 * 1)).  For test cases, the range will be between 1 and 18.
+
+// Function - SimpleAdding(num)
+// The function will take the num parameter and add up all the numbers from 1 to num.  For test cases, the parameter num will be any number from 1 to 1000.
+
+// Function - LetterCapitalize(str)
+// The function will take the str parameter being passed and capitalize the first letter of each word.  Words will be separated by only one space.
+
+// Function rangeRover(arr)
+// The function will take an array of two numbers and return the sum of those two numbers AND all numbers between them.  The lowest number will not always come first.  For example rangeRover([1, 4]) should return 10, i.e.(1 + 2 + 3 + 4), rangeRover([4, 1]) should also return 10.
+
+// Function missingLetter(str)
+// The function will find the missing letter passed in the parameter and return it.  If all letters are present in the string, the return will be undefined.  For example missingLetter("abce") should return "d", missingLetter("bcd") should return undefined.
+
+// Function hailCaesar(num)
+// The function will take the num parameter and convert the given number into a roman numeral.  For example hailCaesar(2) should return "II", hailCaesar(5) should return "V".
+
+// Function spinalTap(str)
+// The function will convert a string to spinal case.  Spinal case is all-lowercase-words-joined-by-dashes.  For example spinalTap("I own this Taco Stand!") should return "i-own-this-taco-stand!".
+
+// Function sumFibs(num)
+// The function will return the sum of all ODD Fibonacci numbers up to and including the passed number if it's a Fibonacci number.  The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, where the next number is found by adding up the two numbers before it.  For example subFibs(4) should return 5, sumbFibs(1000) should return 1785.
